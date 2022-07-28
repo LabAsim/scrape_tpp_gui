6ddc3b4086e9d2e0b49679440aeb56a70cbbfa74
Lab <103599920+LabAsim@users.noreply.github.com>
2022-07-28 21:33:36 +0300

    Create scrape_tpp_gui_pyinstaller.py

---
+++ scrape_tpp_gui_pyinstaller.py
@@ -0,0 +1,17 @@
+import os
+import PyInstaller.__main__
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+name = sys.argv[0].split("/")[-1].replace("_pyinstaller", "")
+images_dir_path = os.path.join(dir_path, 'images')
+source_dir_path = os.path.join(dir_path, 'source')
+
+PyInstaller.__main__.run([
+    f'{name}',
+    '--onedir',
+    '--nowindowed',
+    '--noconsole',
+    f'--add-data={images_dir_path};images',
+    f'--add-data={source_dir_path};source',
+    '-y'
+])
