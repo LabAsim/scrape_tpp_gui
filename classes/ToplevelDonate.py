fbf2156a3c7f59e1cc6816da6ba910e65e065030
LabAsim <lampgit@pm.me>
2022-10-25 22:34:42 +0300

    tktooltips in TopLevelDonate

--- classes/ToplevelDonate.py
+++ classes/ToplevelDonate.py
@@ -1,7 +1,13 @@
 import os
+import sys
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import tktooltip
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+# Now, python can detect the helper_functions.py from the parent directory
 from helper_functions import file_exists, center, callback
 
 
@@ -25,6 +31,7 @@ class ToplevelDonate:
         self.label_dorea.image = self.img_dorea
         self.label_dorea.pack(expand=True, fill="both", side='left')
         self.label_dorea.bind('<Button-1>', lambda e: callback(ToplevelDonate.donation_urls[0]))
+        tktooltip.ToolTip(self.label_dorea, msg='Click to make a one-time donation', delay=0.5)
         # Second image
         self.img_monthly = ImageTk.PhotoImage(Image.open(os.path.join(self.dir_path, "mhniaia.jpg")))
         self.label_monthly = ttk.Label(self.topleveldonate, image=self.img_monthly, cursor='hand2')
@@ -33,6 +40,7 @@ class ToplevelDonate:
         self.label_monthly.image = self.img_monthly
         self.label_monthly.pack(expand=True, fill="both", side='left')
         self.label_monthly.bind('<Button-1>', lambda e: callback(ToplevelDonate.donation_urls[1]))
+        tktooltip.ToolTip(self.label_monthly, msg='Click to make a recurrent monthly donation', delay=0.5)
         # Third image
         self.img_annually = ImageTk.PhotoImage(Image.open(os.path.join(self.dir_path, "ethsia.jpg")))
         self.label_annually = ttk.Label(self.topleveldonate, image=self.img_annually, cursor='hand2')
@@ -41,4 +49,13 @@ class ToplevelDonate:
         self.label_annually.image = self.img_annually
         self.label_annually.pack(expand=True, fill="both", side='left')
         self.label_annually.bind('<Button-1>', lambda e: callback(ToplevelDonate.donation_urls[2]))
+        tktooltip.ToolTip(self.label_annually, msg='Click to make a recurrent annual donation', delay=0.5)
         center(self.topleveldonate, root)
+
+
+if __name__ == "__main__":
+    from misc import dir_path
+    root = tk.Tk()
+    center(root)
+    ToplevelDonate(controller=None, root=root, dir_path=dir_path)
+    root.mainloop()
