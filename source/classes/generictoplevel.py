203f29928c9f89d7fe659dbe70c4d4c659e0ac65
LabAsim <lampgit@pm.me>
2023-02-01 22:33:39 +0200

    Find path of temp folders and sys exe

--- source/classes/generictoplevel.py
+++ source/classes/generictoplevel.py
@@ -1,16 +1,7 @@
+import ctypes
 import os
 import sys
 import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
-import tktooltip
-
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-parent_parent = os.path.dirname(parent)
-sys.path.append(parent_parent)
-# Now, python can detect the helper_functions.py from the parent directory
-from helper_functions import file_exists, center, callback
 
 
 class GenericToplevel:
@@ -24,19 +15,48 @@ class GenericToplevel:
         self.controller = controller
         self.dir_path = self.find_the_path_of_main()
         self.toplevel = tk.Toplevel()
-        center(self.toplevel, root)
+        self.dir_path_exe = self.find_the_path_of_main_exe()
 
-    def find_the_path_of_main(self) -> str:
+    def find_the_path_of_main_exe(self) -> str:
         """
-        Returns The path of the directory of main.py or the .exe
-        :return: The path of the directory of main.py or the .exe
+        Returns The path of the directory of main.py or .exe
+        :return: The path of the directory of main.py or .exe
 
 
         Note that if the current class is import in App.py as
-        'from scrape_tpp_gui.source.classes.ToplevelSocial import ToplevelSocial',
+        'from scrape_tpp_gui.source.classes.generictoplevel import GenericToplevel',
         the path will contain scrape_tpp_gui\\, which does not exist in temporary directories.
         Thus, in this situation, you need the parent folder.
-        It's easier to use 'source.classes.ToplevelSocial import ToplevelSocial' to avoid that
+        It's easier to use 'source.classes.generictoplevel import GenericToplevel' to avoid that
+        """
+        if getattr(sys, 'frozen', False):
+            print(getattr(sys, 'frozen', False))
+            # The temporary path of the file when the app runs as an .exe
+            self.dir_path_exe = os.path.dirname(os.path.realpath(sys.executable))
+            self.dir_path_exe = os.path.dirname(self.dir_path_exe)  # We need the parent of the parent of this directory
+            # self.dir_path = os.path.dirname(self.dir_path)  # We need the parent of the parent of this directory
+            # If the path until this step contains \\scrape_tpp_gui, get the parent dir, which is a temp dir(MEIPP).
+            # self.dir_path = os.path.dirname(self.dir_path)
+            print(f"{self.name_of_class}>Exe(self.dir_path_exe):", self.dir_path_exe)
+            return self.dir_path_exe
+        elif __file__:
+            self.dir_path_exe = os.path.dirname(__file__)  # We need the parent of the parent of this directory
+            self.dir_path_exe = os.path.dirname(self.dir_path_exe)  # We need the parent of the parent of this directory
+            self.dir_path_exe = os.path.dirname(self.dir_path_exe)
+            print(f'{self.name_of_class}>Script (self.dir_path_exe): {self.dir_path_exe}')
+            return self.dir_path_exe
+
+    def find_the_path_of_main(self) -> str:
+        """
+        Returns The path of the directory of main.py or the temporary folder hosting the files i.e.: `Temp/_MEI71042`
+        :return: The path of the directory of main.py or the temporary folder
+
+
+        Note that if the current class is import in App.py as
+        'from scrape_tpp_gui.source.classes.generictoplevel import GenericToplevel',
+        the path will contain scrape_tpp_gui\\, which does not exist in temporary directories.
+        Thus, in this situation, you need the parent folder.
+        It's easier to use 'source.classes.generictoplevel import GenericToplevel' to avoid that
         """
         if getattr(sys, 'frozen', False):
             print(getattr(sys, 'frozen', False))
@@ -46,13 +66,13 @@ class GenericToplevel:
             self.dir_path = os.path.dirname(self.dir_path)  # We need the parent of the parent of this directory
             # If the path until this step contains \\scrape_tpp_gui, get the parent dir, which is a temp dir(MEIPP).
             # self.dir_path = os.path.dirname(self.dir_path)
-            print("Exe:", self.dir_path)
+            print(f"{self.name_of_class}>Exe (self.dir_path):", self.dir_path)
             return self.dir_path
         elif __file__:
             self.dir_path = os.path.dirname(__file__)  # We need the parent of the parent of this directory
             self.dir_path = os.path.dirname(self.dir_path)  # We need the parent of the parent of this directory
             self.dir_path = os.path.dirname(self.dir_path)
-            print(f'Script: {self.dir_path}')
+            print(f'{self.name_of_class}>Script (self.dir_path): {self.dir_path}')
             return self.dir_path
 
     def bring_focus_back(self) -> None:
